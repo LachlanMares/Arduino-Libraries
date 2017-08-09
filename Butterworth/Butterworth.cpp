4bd40e350107d9e2ffd941946bf6631d860037ee
LachlanMares <30546646+LachlanMares@users.noreply.github.com>
2017-08-09 15:06:18 +1000

    Add files via upload

---
+++ Butterworth/Butterworth.cpp
@@ -0,0 +1,36 @@
+#include "Butterworth.h"	
+
+Butterworth::Butterworth(){
+
+ }
+
+void Butterworth::Init(float SampleFreq,float CutoffFreq,float InitVal){
+  float _Wc = TWO_M_PI*CutoffFreq;
+  float _c = 1.0f/(tan((_Wc*(1.0f/SampleFreq))/2.0f));
+  float _G = _c*_c + SQRT2*_c + 1.0f;
+  
+  _Yc[0] = (2.0f - 2.0f*_c*_c)/_G;
+  _Yc[1] = (_c*_c - SQRT2*_c + 1.0f)/_G;
+  _Xc[0] = 1.0f/_G;
+  _Xc[1] = 2.0f/_G;
+  _Xc[2] = 1.0f/_G;
+  
+  for(int _i=0;_i<3;_i++){
+    _x[_i] = InitVal;
+    _y[_i] = InitVal;
+  }
+}
+
+float Butterworth::UpdateFilter(float NewReading){
+  _x[2] = _x[1];
+  _x[1] = _x[0];
+  _x[0] = NewReading;
+  _y[2] = _y[1];
+  _y[1] = _y[0]; 
+  _y[0] = _Xc[0]*_x[0] + _Xc[1]*_x[1] + _Xc[2]*_x[2] - _Yc[0]*_y[1] - _Yc[1]*_y[2];
+  return _y[0];
+}
+
+float Butterworth::Output(){
+  return _y[0];
+}
---
+++ Butterworth/Butterworth.h
@@ -0,0 +1,20 @@
+#ifndef Butterworth_h
+#define Butterworth_h
+#include "Arduino.h"
+
+#define M_PI                3.141592653589793238f               // Pi
+#define TWO_M_PI            6.283185307179586477f               // Pi * 2
+#define SQRT2               1.414213562373095048f               // Square root of 2 
+
+class Butterworth
+{
+public:
+	Butterworth();
+        void Init(float,float,float);
+        float UpdateFilter(float);
+        float Output();
+private:
+        float _x[3],_y[3],_Xc[3],_Yc[3];
+};
+
+#endif
