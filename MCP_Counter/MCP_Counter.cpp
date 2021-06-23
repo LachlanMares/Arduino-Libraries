c4e1c46fc6c8663299b0db95559408e7f796d05b
LachlanMares <30546646+LachlanMares@users.noreply.github.com>
2021-06-23 19:40:26 +0930

    Add files via upload

---
+++ MCP_Counter/MCP_Counter.cpp
@@ -0,0 +1,108 @@
+#include "MCP_Counter.h"
+
+MCP_Counter::MCP_Counter(){
+  MCP23017 _mcp;
+}
+
+void MCP_Counter::Initialise(int MCP_Addr){
+  _mcp.Begin(MCP_Addr);
+  _mcp.SetDirection('A',MCP_INPUTS);
+  _mcp.SetPullUp('A',MCP_PULLUP_ALL);
+  
+  _mcp.SetDirection('B',MCP_OUTPUTS); 
+  _mcp.WriteIOBank('B',0xFE);
+  delay(1);
+  _mcp.WriteIOBit('B',CCLR,true);
+  _mcp.WriteIOBit('B',RCLK,false);  
+
+  _LastMillisC0 = millis();
+ 
+  _LastCounter0Value = 0;
+  _LastCounter1Value = 0;
+  _LastMillisC1 = _LastMillisC0;
+  _LastCounter0Value = 0;    
+}
+
+void MCP_Counter::ReadCounter16Bit(char Bank,unsigned int* CounterValue,unsigned int* Difference,unsigned int* Period){
+  unsigned long _MillisNow = millis();
+  *CounterValue = Read16Counter(Bank);
+  switch(Bank){
+    case 'A':
+      *Period = (unsigned int)(_MillisNow - _LastMillisC0);
+      _LastMillisC0 = _MillisNow;
+      if(*CounterValue >= _LastCounter0Value){
+        *Difference = *CounterValue - _LastCounter0Value;
+      } else {
+          *Difference = (65535 - _LastCounter0Value) + *CounterValue;
+        }
+      _LastCounter0Value = *CounterValue;
+      break;
+    case 'B':
+      *Period = (unsigned int)(_MillisNow - _LastMillisC1);
+      _LastMillisC1 = _MillisNow;
+      if(*CounterValue >= _LastCounter1Value){
+        *Difference = *CounterValue - _LastCounter1Value;
+      } else {
+          *Difference = (65535 - _LastCounter1Value) + *CounterValue;
+        }
+      _LastCounter1Value = *CounterValue; 
+      break;
+  }
+}
+
+void MCP_Counter::ReadCounter32Bit(unsigned long* CounterValue,unsigned long* Difference,unsigned int* Period){
+  unsigned long _MillisNow = millis();
+  *CounterValue = Read32Counter();
+  *Period = (unsigned int)(_MillisNow - _LastMillisC0);
+  _LastMillisC0 = _MillisNow;
+  if(*CounterValue >= _LastCounter0Value){
+        *Difference = *CounterValue - _LastCounter0Value;
+      } else {
+          *Difference = (4294967295 - _LastCounter0Value) + *CounterValue;
+        }
+  _LastCounter0Value = *CounterValue; 
+}
+
+unsigned int MCP_Counter::Read16Counter(unsigned char Bank){
+   unsigned int _TempCounter = 0;  
+  _mcp.WriteIOBit('B',RCLK,true);
+  switch(Bank){
+    case 'A':
+      _mcp.WriteIOBank('B',0xEF);
+      _TempCounter = _mcp.ReadIOBank('A') << 8;
+      _mcp.WriteIOBank('B',0xFF); 
+      _mcp.WriteIOBank('B',0xDF);
+      _TempCounter += _mcp.ReadIOBank('A');
+      _mcp.WriteIOBank('B',0xFF);
+      break;
+    case 'B':
+      _mcp.WriteIOBank('B',0xFB);
+      _TempCounter = _mcp.ReadIOBank('A') << 8;
+      _mcp.WriteIOBank('B',0xFF);  
+      _mcp.WriteIOBank('B',0xF7);
+      _TempCounter += _mcp.ReadIOBank('A');
+      _mcp.WriteIOBank('B',0xFF); 
+      break;
+  }
+  _mcp.WriteIOBit('B',RCLK,false);
+  return _TempCounter;
+}
+
+unsigned long MCP_Counter::Read32Counter(){
+  unsigned long _TempCounter = 0;
+  _mcp.WriteIOBit('B',RCLK,true);
+  _mcp.WriteIOBank('B',0xFB);
+  _TempCounter = (unsigned long)(_mcp.ReadIOBank('A')) << 24;
+  _mcp.WriteIOBank('B',0xFF); 
+  _mcp.WriteIOBank('B',0xF7);
+  _TempCounter += (unsigned long)(_mcp.ReadIOBank('A')) << 16;
+  _mcp.WriteIOBank('B',0xFF);
+  _mcp.WriteIOBank('B',0xEF);
+  _TempCounter += _mcp.ReadIOBank('A') << 8;
+  _mcp.WriteIOBank('B',0xFF); 
+  _mcp.WriteIOBank('B',0xDF);
+  _TempCounter += _mcp.ReadIOBank('A');
+  _mcp.WriteIOBank('B',0xFF);
+  _mcp.WriteIOBit('B',RCLK,false);
+  return _TempCounter;  
+}
---
+++ MCP_Counter/MCP_Counter.h
@@ -0,0 +1,30 @@
+#ifndef MCP_Counter_h
+#define MCP_Counter_h
+#include "Arduino.h"
+#include <MCP23017.h>
+
+// SN74LV8154 Pins MCP port 1
+#define GAL                  5  
+#define GAU                  4  
+#define GBL                  3  
+#define GBU                  2  
+#define RCLK                 1  
+#define CCLR                 0 
+
+class MCP_Counter
+{
+public:
+	MCP_Counter();
+        void Initialise(int);
+        void ReadCounter16Bit(char,unsigned int*,unsigned int*,unsigned int*);
+        void ReadCounter32Bit(unsigned long*,unsigned long*,unsigned int*);
+private:
+        unsigned int _LastCounter0Value,_LastCounter1Value,_Counter0Value,_Counter1Value;
+        unsigned long _LastCounter32BitValue,_LastMillisC0,_LastMillisC1;
+        MCP23017 _mcp;  
+        
+        unsigned int Read16Counter(unsigned char);
+        unsigned long Read32Counter();
+};
+
+#endif
